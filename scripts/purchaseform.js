fe12112fb22d32d964069b30ba0bea528c198452
= <=>
2025-10-06 20:12:13 -0600

    Revamped range options function. Linked to previous page in probably the hackiest way possible.

--- scripts/purchaseform.js
+++ scripts/purchaseform.js
@@ -1,27 +1,46 @@
 var prev = window.location.search
-var plan = new URLSearchParams(prev)
+var prevdata = new URLSearchParams(prev)
+plan = prevdata.get("plan")
+console.log(plan)
 planmenu = document.getElementById("plans")
-planmenu.setAttribute("value",plan)
-function rangeControl(caller){
+planmenu.value = String(plan)
+
+
+
+function rangeControl(){
     houseoptions = [["1-4",.33],["4-10",.66],["More than 10",1],["Prefer not to say",1]]
     corpoptions = [["20-50",.33],["50-70",.66],["More than 70",1],["Prefer not to say",1]]
     globaloptions = [["1000 or less",.33],["10000-50000",.66],["More than 50000",1],["Prefer not to say",1]]
-    if (planmenu.value == "house") {
-        var options = houseoptions
+    var options = []
+    peoplerange = document.getElementById("peoplerange")
+    console.log("Function called")
+    peoplerange.innerHTML = "<p>How many people on your team?</p>"
+    if (planmenu.value == "household") {
+        options = houseoptions
     }
-    else if (planmenu.value == "company") {
+    else if (planmenu.value == "business") {
          options = corpoptions
     }
     else if (planmenu.value == "global") {
         options = globaloptions
     }
-    var i = 0
-    for (thing in caller) {
-        if (thing.type == "input") {
-            thing.labels[0].innerHTML = options[i][0]
-            thing.setAttribute("value", options[i][1])
-            i++
+    
+    console.log("Array set!")
+    console.log(options)
+    newform = []
+    for (var i = 0; i< options.length; i++) {
+         var thing = document.createElement("input")
+         var thinglabel = document.createElement("label")
+         thing.setAttribute("name","teamsize")
+         thing.setAttribute("type","radio")
+         thing.setAttribute("id",i)
+         thing.setAttribute("value",options[i][1])
+         thinglabel.setAttribute("for",i)
+         thinglabel.textContent = (options[i][0] + " ")
+         peoplerange.append(thing)
+         peoplerange.append(thinglabel)  
         }
-
+        
     }
-}
+
+    
