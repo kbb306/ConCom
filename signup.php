1bf0f6bfec27899df1b4020e8319a598d276da24
kbb306 <kylerbbelshaw@gmail.com>
2025-11-05 14:09:53 -0700

    Created upload()

--- signup.php
+++ signup.php
@@ -1,5 +1,10 @@
 <php? 
-    $username = "root"
-    $password = "Legally18"
-    $servername = "localhost"
-    $dbname
+function upload($name,$pass,$domain,$plan,$servers,$price,$access) {
+    $username = "root";
+    $password = "Legally18";
+    $servername = "localhost";
+    $dbname = "concom";
+    $conn = new mysqli($servername,$username,$password,$dbname);
+    $sql = "INSERT INTO UserData $name $pass $domain $plan $servers $price $access True";
+}
+?>
