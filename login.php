fbe5c2be272f60d13a45bbd82c1f8d2df3541b0c
kbb306 <kylerbbelshaw@gmail.com>
2025-11-26 13:53:32 -0700

    Fixed select statement

--- login.php
+++ login.php
@@ -7,26 +7,29 @@
             <label>Enter your password: &nbsp; <input type="password" name="password"></label>
         </form> 
         <?php
-            session_start()
+            session_start();
             $username = "root";
             $password = "Legally18";
             $servername = "localhost";
             $dbname = "concom";
             $conn = new mysqli($servername,$username,$password,$dbname);
+            if (isset($_POST["notEmail"],$_POST["password"])) {
             $notEmail = $_POST["notEmail"];
             $password = $_POST["password"];
             $addrarray = explode("@",$notEmail);
             $name = $addrarray[0];
             $domain = $addrarray[1];
-            $sql = "SELECT 'password' FROM 'UserData' WHERE 'name' = $name AND $domain";
+            $sql = "SELECT password FROM UserData WHERE name = '$name' AND domainname = '$domain' ";
             $result = $conn -> query($sql);
             while ($row = $result -> fetch_assoc()) {
                 $pass = $row["password"];
-                if (hash($password) = $pass) {
-                    header("location: account.php")
-                    exit()
+                $password = hash("sha256",$password);
+                if ($password = $pass) {
+                    header("location: account.php");
+                    exit();
                 }
             }
+        }
             ?>
     </body>
 </html> 
