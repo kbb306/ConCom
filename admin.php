d6402d3004abbd42ca44987ef05855863aa89846
kbb306 <kylerbbelshaw@gmail.com>
2025-12-01 19:18:22 -0700

    Added printout of all userdata

--- admin.php
+++ admin.php
@@ -1,5 +1,41 @@
 <html>
+    <head>
+        <title>
+
+        </title>
+        <link rel="stylesheet" src="css/styles.css">
+    </head>
     <body>
-        
+        <?php 
+            $cols = [];
+            $username = "root";
+            $password = "Legally18";
+            $servername = "localhost";
+            $dbname = "concom";
+            $conn = new mysqli($servername,$username,$password,$dbname);
+            $query = "SELECT * FROM userdata JOIN billingdata ON userdata.email = billingdata.email ";
+            $result = $conn -> query($query);
+            if ($result -> num_rows > 0) {
+                print "<h2>User data</h2>";
+                print "<div style=" ."height: 200px; overflow-y: auto;".">
+                            <table>";
+                print "         <tr>";
+                while ($fieldinfo = $result -> fetch_field()) {
+                    print "         <th>" .$fieldinfo->name. "</th>";
+                    
+                }   
+                    print "     </tr>";
+                while ($row = $result -> fetch_assoc()) {
+                    print "     <tr>";
+                    foreach($row as $value) {
+                        print "     <td>". $value . "</td>";
+                    }
+                    print "     </tr>";
+                }        
+                print "     </table>
+                       </div>";
+            }
+            
+        ?>
     </body>
 </html>
