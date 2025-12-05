57c25d43a4d7c061490cccf62259f05926baf25c
kbb306 <kylerbbelshaw@gmail.com>
2025-12-05 02:43:35 -0700

    CSS Changes

--- sales.php
+++ sales.php
@@ -9,7 +9,7 @@
 <?php include("nav.html");?>
     <form action="purchaseform.php">
         <div class="plan-grid">
-            <div id="Household">
+            <div id="Household" class="plan">
                 <h2>Household</h2>
                 <figure>
                 <img src="images/ConCom_house.jpg" width="75%">
@@ -18,7 +18,7 @@
                 <a href="purchaseform.php?plan=household">Choose</a>
             </div>
 
-            <div id="Business">
+            <div id="Business" class="plan">
                 <h2>Business</h2>
                 <figure>
                 <img src="images/ConCom_Office.jpg" width="75%">
@@ -27,7 +27,7 @@
                 <a href="purchaseform.php?plan=business">Choose</a>
             </div>
 
-            <div id="Global">
+            <div id="Global" class="plan">
                 <h2>Global</h2>
                 <figure>
                 <img src="images/ConCom_Earth.jpg" width="75%">
@@ -35,8 +35,8 @@
                 </figure>
                 <a href="purchaseform.php?plan=global">Choose</a>
             </div>
-            <span class="disclaimer">In addition to the prices listed on the submenus, ConCom reserves the right to use spare processing power and storage on all connected client systems.</span>
         </div>
     </form>
+    <span class="disclaimer">In addition to the prices listed on the submenus, ConCom reserves the right to use spare processing power and storage on all connected client systems.</span>
 </body>
 </html>
